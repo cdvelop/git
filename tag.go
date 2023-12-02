ab2b793b048d156c29341825f24bb9d8f219b253
Cesar Solis <44058491+cdvelop@users.noreply.github.com>
2023-12-02 12:27:52 -0300

    init code

---
+++ tag.go
@@ -0,0 +1,17 @@
+package git
+
+import (
+	"os/exec"
+	"strings"
+)
+
+func GetLatestTag() (tag, err string) {
+	cmd := exec.Command("git", "describe", "--tags", "--abbrev=0")
+	output, er := cmd.Output()
+	if er != nil {
+		return "", "GetLatestTag error " + er.Error()
+	}
+
+	// Convierte el resultado en una cadena y elimina espacios en blanco adicionales
+	return strings.TrimSpace(string(output)), ""
+}
